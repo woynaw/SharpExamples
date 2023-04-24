4d9cbc47d05374d59494080edc75941f958d329a
Войналович Владислав <woynaw@hotmail.com>
2023-04-24 15:45:21 +0300

    finish if else

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -6,3 +6,11 @@ int e = 4;
 
 int max = a;
 
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.Write(max);
