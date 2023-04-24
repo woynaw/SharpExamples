7c610b5ae21c490d819bffc6b8d8905c1cd373b5
woynaw <woynaw@yandex.ru>
2023-04-24 21:03:38 +0300

    add Session task

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -13,7 +13,7 @@ int result = MaxDigit(num);
 Console.WriteLine($"{num} -> {result}");
 */
 
-bool Kratnost(int num1, int num2)
+/* bool Kratnost(int num1, int num2)
 {
     if (num1 % num2 == 0)
     {
@@ -32,3 +32,43 @@ Console.WriteLine("Введите второк число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"{num1}, {num2} -> {Kratnost(num1, num2)}, остаток {num1 % num2}");
+*/
+
+//###################################
+/*
+
+bool Kratnost(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} -> {Kratnost(num)}");
+
+*/
+
+bool Square(int num1, int num2)
+{
+    if (num1 == num2 * num2 || num2 == num1 * num1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num1}, {num2} -> {Square(num1, num2)}");
