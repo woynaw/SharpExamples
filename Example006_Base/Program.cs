d3407ce522c255a6125245cd72a7bd33186e4cce
Войналович Владислав <woynaw@hotmail.com>
2023-04-24 16:11:17 +0300

    fix

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -1,7 +1,7 @@
 ﻿int a = 1;
-int b = 2; 
+int b = 2;
 int c = 6;
-int d = 8; 
+int d = 8;
 int e = 4;
 
 int max = a;
--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -22,22 +22,26 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while (count < 10000) {
+while (count < 10000)
+{
     int what = new Random().Next(0, 3);
-    if (what == 0) {
+    if (what == 0)
+    {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
 
-    if (what == 1) {
+    if (what == 1)
+    {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
-    }    
+    }
 
-    if (what == 2) {
+    if (what == 2)
+    {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
-    }     
+    }
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
