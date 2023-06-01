592a90e50f21e62ca57d0918a5e87c30f168604d
Войналович Владислав <woynaw@hotmail.com>
2023-06-01 14:56:19 +0300

    add Recursion+2D

---
+++ Example013_Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+string[,] table = new string[2, 5];
+// String.Empty;
+// table[0,0] table[0,1] table[0,2] table[0,3] table[0,4]
+// table[1,0] table[1,1] table[1,2] table[1,3] table[1,4]
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows,columns]}-");
+    }
+} */
+int[,] matrix = new int[3, 4];
+void PrintArray(int[,] arg)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(matrix);
