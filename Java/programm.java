8c89b3d9e811a9fa827266d611c85c0eead788b0
Войналович Владислав <woynaw@hotmail.com>
2023-09-28 12:01:57 +0300

    java +

--- Java/programm.java
+++ Java/programm.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 
 public class programm {
     public static void main(String[] args) {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.printf("name: ");
-        String name = iScanner.nextLine();
-        System.out.printf("Привет , %s!", name);
-        iScanner.close();
-        
+        // Scanner iScanner = new Scanner(System.in);
+        // System.out.printf("name: ");
+        // String name = iScanner.nextLine();
+        // System.out.printf("Привет , %s!", name);
+        // iScanner.close();
+        // String s = "qwe";
+        // int a = 123;
+        // String q = s + a;
+        // System.out.println(q);
+        lib.sayHi();
+    }
+    static void sayHi() {
+        System.out.println("hi");
     }
 }
